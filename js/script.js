09f50b42266031e44846379a487a388857f8e649
Jonathan <jo@MacBook-Pro-de-Jonathan.local>
2022-08-16 14:46:23 -0400

    im too tired damn

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const savedLegends = localStorage.getItem('savedLegends');
 
 if (savedLegends) {
-    Object.keys(JSON.parse(savedLegends)).length !== Object.keys(JSON.parse(Characters)).length ?
+    Object.keys(JSON.parse(savedLegends)).length !== Object.keys(Characters).length ?
         createHero(Characters)
         :
         createHero(JSON.parse(savedLegends))
